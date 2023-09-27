6715d36874d5de648e548ea95556f10ed724172c
sanmofe <santiagomorafelix@gmail.com>
2023-09-26 22:12:43 -0500

    Tutorial terminado con las clases de la tabla pero sin estilos

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import Job from "./job";
+import { FormattedMessage, useIntl } from "react-intl";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +16,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 100000000,
     },
     {
       id: "0002",
@@ -18,6 +25,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 100000000,
     },
     {
       id: "0003",
@@ -26,20 +34,34 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 100000000,
     },
   ]);
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+        <thead class="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="PublicationDate" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Views" />
+            </th>
           </tr>
         </thead>
         <tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+    <IntlProvider locale={navigator.language} messages= {navigator.language.startsWith("es")?localeEsMessages:localeEnMessages}>
+            <JobsList/>
+    </IntlProvider>, document.getElementById("root")
+);
